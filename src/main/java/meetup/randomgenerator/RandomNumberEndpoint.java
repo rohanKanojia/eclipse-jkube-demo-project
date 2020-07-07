5fe01033fc4db0bf6d29d988d4c35e39b819f272
Rohan Kumar <rohaan@redhat.com>
2020-07-07 21:45:44 +0530

    Replace @RequestMapping with @GetMapping

--- src/main/java/meetup/randomgenerator/RandomNumberEndpoint.java
+++ src/main/java/meetup/randomgenerator/RandomNumberEndpoint.java
@@ -5,22 +5,19 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author roland
- * @since 12.10.17
- */
 @RestController
 public class RandomNumberEndpoint {
-
     private static final UUID id = UUID.randomUUID();
 
-    private Random random = new Random();
-
-    @RequestMapping(value = "/random", produces = "application/json")
+    @GetMapping(value = "/random", produces = "application/json")
     public Map<String, String> random() {
+        return getRandomMessage();
+    }
+
+    private Map<String, String> getRandomMessage() {
         Map<String, String> ret = new HashMap<>();
         ret.put("id", id.toString());
         return ret;
