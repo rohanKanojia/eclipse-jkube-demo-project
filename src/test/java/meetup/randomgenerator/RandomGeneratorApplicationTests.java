97b00fbb82f41281a51657739d399d7bff95a2ae
Rohan Kumar <rohaan@redhat.com>
2020-07-07 22:07:13 +0530

    Add basic test

--- src/test/java/meetup/randomgenerator/RandomGeneratorApplicationTests.java
+++ src/test/java/meetup/randomgenerator/RandomGeneratorApplicationTests.java
@@ -1,9 +1,12 @@
 package meetup.randomgenerator;
 
+import meetup.randomgenerator.model.RandomNumberEndpointResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,13 @@ public class RandomGeneratorApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		// Given
+
+		// When
+		RandomNumberEndpointResponse result = RandomNumberEndpoint.getRandomMessage();
+
+		// Then
+		assertNotNull(result);
 	}
 
 }
